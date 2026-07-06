56431d840e507ca68b67b62736111ac91da3e332
DrJielun <jie_lun_tan@ite.edu.sg>
2026-07-06 20:44:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
-import numpy as npa
+import numpy as np
 
 # --- 1. CONFIGURATION & APP INITIALIZATION ---
 st.set_page_config(layout="wide", page_title="Institutional Quant Terminal")
